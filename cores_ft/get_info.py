adce5f7b8ae8026960a8a484a7eb4eedc0cd9a7f
Henrique Andrade <henrique.ccda@gmail.com>
2022-02-27 00:49:10 -0300

    Testes criados.
     close #3

--- cores_ft/get_info.py
+++ cores_ft/get_info.py
@@ -3,15 +3,16 @@ import os
 
 def read_cpuinfo():
     # read freq from /proc/cpuinfo
+
     with open('/proc/cpuinfo') as f:
         lines = f.readlines()
 
     i, freq = 0, {}
-    for l in lines:
-        cols = l.split(':')
+    for lin in lines:
+        cols = lin.split(':')
         if cols[0].startswith('cpu MHz'):
             freq[f'core_{i}'] = float(cols[1])/1000
-            i+=1
+            i += 1
 
     return freq
 
@@ -20,14 +21,14 @@ def get_lm_sensors_info():
     # get temp using sensors
     os.system('sensors > tmp')
     with open('tmp') as f:
-        lines =f.readlines()
+        lines = f.readlines()
 
     i, temp = 0, {}
-    for l in lines:
-        cols = l.split(':')
+    for lin in lines:
+        cols = lin.split(':')
         if cols[0].startswith('Core'):
             temp[f'core_{i}'] = cols[1].split()[0]
-            i+=1
+            i += 1
     os.system('rm tmp')
 
     return temp
