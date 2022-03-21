3b0897e1b25635e64082eb0060f7626f12b3f8db
Henrique Andrade <henrique.ccda@gmail.com>
2022-03-20 21:48:05 -0300

    Impressão da frequência e da temperatura modificada.
    close #8

--- cores_ft/output_msg.py
+++ cores_ft/output_msg.py
@@ -1,7 +1,12 @@
 def mount_msg_output(freq, temp):
     msg = []
-    for (k, f), t in zip(freq.items(), temp.values()):
-        msg.append(f'| {k} : {f:.4f} Ghz {t} |')
-    msg = '\n'.join(msg)
 
-    return msg
+    if freq and temp:
+        for (k, f), t in zip(freq.items(), temp.values()):
+            msg.append(f'| {k} : {f:.4f} Ghz {t} |')
+
+    elif not temp:
+        for (k, f) in freq.items():
+            msg.append(f'| {k} : {f:.4f} Ghz |')
+
+    return '\n'.join(msg)
